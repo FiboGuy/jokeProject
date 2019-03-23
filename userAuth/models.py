b52fe9a44ea52ddd5f4aa47903f3613690f56dd8
Cristian Tejedor <Tejedor92@gmail.com>
2019-03-23 22:35:49 +0100

    user auth completed with image defualt

--- userAuth/models.py
+++ userAuth/models.py
@@ -6,8 +6,6 @@ from django.dispatch import receiver
 import os
 # Create your models here.
 
-
-
 def user_directory_path(instance, filename):
     return 'user_{0}/{1}'.format(instance.user.username, filename)
 
@@ -21,15 +19,20 @@ def validate_image(value):
 
 class Profile(models.Model):
     user=models.OneToOneField(User, on_delete=models.CASCADE)
-    image=models.ImageField(upload_to=user_directory_path, validators=[validate_image], default=None)
+    image=models.ImageField(upload_to=user_directory_path, validators=[validate_image], default='default/profile.png')
     # active boolean field for email activation account.
 
     def __str__(self):
         return self.user.username
+
+    class Meta:
+        db_table='profile'
+
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
     
+
 
